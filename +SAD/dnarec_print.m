87843317237738cb3898b42aaf6aea7e6bb4964e
albertas albertas <albdvir@gmail.com>
2025-03-21 12:51:08 +0100

    fix warning when having . in filepath

--- +SAD/dnarec_print.m
+++ +SAD/dnarec_print.m
@@ -62,7 +62,7 @@ function printName = dnarec_print(output, sets, runNo,filtered)
 
   % Print overall results
   fid = fopen(printName, 'w');
-  fprintf(fid, 'Results for the analysis of %s\n', printName(1:strfind(printName, '.') - 1));
+  fprintf(fid, 'Results for the analysis of %s\n', printName(1:strfind(printName, '.','last') - 1));
   fprintf(fid, '\n Total number of barcodes: %i \n', sum(imBars));
   fprintf(fid, '\n Total length of barcodes: %.1f micrometer \n', sum(imBarLength));
   fprintf(fid, '\n Average length of barcodes: %.1f micrometer \n', mean(horzcat(imBarLengthAll{:})));
